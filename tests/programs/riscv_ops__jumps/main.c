e9652728a82d22d689c1dff8e67f0e5fc3aec836
Tom Herman <tomherman4@gmail.com>
2025-01-04 19:19:18 +0200

    beq,bne, blt,bge, bltu,bgeu tested! bgeu fixed!

--- tests/programs/riscv_ops__jumps/main.c
+++ tests/programs/riscv_ops__jumps/main.c
@@ -2,6 +2,51 @@
 #include "c2fj_syscall.h"
 
 
+#define PRINT_A2 "jal a2, .+22\n"
+
+
+/// Compares in this order beq,bne, blt,bge, bltu,bgeu
+/// Foreach: print 1 if taken, 0 if not taken.
+void compare_with_numbers(int32_t a0, int32_t a1) {
+asm volatile (
+        "mv a0, %0\n"
+        "mv a1, %1\n"
+
+        "addi a2, zero, 0x1\n"
+        "beq a0, a1, 1f\n"
+        "xor a2, a2, a2\n"
+        "1: " PRINT_A2
+
+        "addi a2, zero, 0x1\n"
+        "bne a0, a1, 2f\n"
+        "xor a2, a2, a2\n"
+        "2: " PRINT_A2
+
+        "addi a2, zero, 0x1\n"
+        "blt a0, a1, 3f\n"
+        "xor a2, a2, a2\n"
+        "3: " PRINT_A2
+
+        "addi a2, zero, 0x1\n"
+        "bge a0, a1, 4f\n"
+        "xor a2, a2, a2\n"
+        "4: " PRINT_A2
+
+        "addi a2, zero, 0x1\n"
+        "bltu a0, a1, 5f\n"
+        "xor a2, a2, a2\n"
+        "5: " PRINT_A2
+
+        "addi a2, zero, 0x1\n"
+        "bgeu a0, a1, 6f\n"
+        "xor a2, a2, a2\n"
+        "6: " PRINT_A2
+
+    ::"r"(a0), "r"(a1) : "a0", "a1", "a2", "memory");
+    c2fj_print_char('\n');
+}
+
+
 int main() {
     // TODO test all 6 branch opcodes.
     int32_t val;
@@ -20,8 +65,8 @@ int main() {
         "addi a0, a0, 0x400\n"
         "1: sub a0, a0, a1\n"
         "lui a1, 0x123\n"
-        "sub a1, a1, a0\n"
-        "jal a1, .+22\n"    // prints 0x8
+        "sub a2, a1, a0\n"
+        PRINT_A2            // prints 0x8
     :::"memory");
     c2fj_print_char('\n');
 
@@ -42,21 +87,21 @@ int main() {
         "addi a2, a2, 1\n"
         "addi a2, a2, 1\n"
         "addi a2, a2, 1\n"
-        "jal a2, .+22\n"    // prints 0x5
+        PRINT_A2            // prints 0x5
         "sub a2, a0, a1\n"
-        "jal a2, .+22\n"    // prints 0x10
+        PRINT_A2            // prints 0x10
     :::"memory");
     c2fj_print_char('\n');
 
-//        asm volatile (
-//        "xor a2, a2, a2\n"
-//        "lui a0, 0x123\n"
-//        "lui a1, 0x123\n"
-//        "beq a0, a1, 5f\n"
-//        "lui a2, 0x123"
-//        "5:"
-//    :::"memory");
-//    c2fj_print_char('\n');
+    compare_with_numbers(0x123, 0x123);
+    compare_with_numbers(0x234, 0x123);
+    compare_with_numbers(0x123, 0x234);
+
+    compare_with_numbers(-0x123, 0x234);
+    compare_with_numbers(-0x234, 0x123);
+
+    compare_with_numbers(-0x123, -0x234);
+    compare_with_numbers(-0x234, -0x123);
 
     return 0;
 }
