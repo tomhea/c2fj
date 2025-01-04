39152a5106543a5b03168a8a98adced5ea8d94ff
Tom Herman <tomherman4@gmail.com>
2025-01-04 15:40:17 +0200

    add primes test

--- tests/programs/hello_input/main.c
+++ tests/programs/hello_input/main.c
@@ -6,7 +6,7 @@
 int main() {
     char name[20] = {0};
     printf("Enter your name: ");
-    scanf("%19s", name);
+    scanf("%19s", name);  // TODO make it work! It currently only work with length up to "%8s".
     printf("Hello %s!\n", name);
     return 0;
 }
---
+++ tests/programs/primes/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+int main() {
+    int max_number;
+    scanf("%d", &max_number);
+
+    if (max_number < 0) {
+        return 1;
+    }
+    if (max_number <= 1) {
+        return 0;
+    }
+    printf("2\n");
+
+    bool* non_prime = malloc(max_number + 1);
+    for (int i = 0; i <= max_number; i++) {
+        non_prime[i] = false;
+    }
+
+    for (int p = 3; p <= max_number; p += 2) {
+        if (non_prime[p] == false) {
+            for (int i = p*p; i <= max_number; i += p) {
+                non_prime[i] = true;
+            }
+            printf("%d\n", p);
+        }
+    }
+
+    return 0;
+}
